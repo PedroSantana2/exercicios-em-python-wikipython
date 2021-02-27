943f892684921834ded193c2eccf02f0e2b86b46
Pedro Santana <79723658+PedroSantana2@users.noreply.github.com>
2021-02-27 17:14:29 -0300

    Terceiro exercício

---
+++ primeira_lista/ex3.py
@@ -0,0 +1,6 @@
+'''
+Faça um Programa que peça dois números e imprima a soma.
+'''
+num1 = int(input('Digite o primeiro número da soma: '))
+num2 = int(input('Digite o segundo número da soma: '))
+print('A soma dos números {} e {} é: {}'.format(num1, num2, num1 + num2))
